c66a1de359e21ca36064cc064a5ab188ea394636
Philip Orvis <orvisuals@gmail.com>
2020-08-07 16:37:32 -0500

    applied security

--- server.js
+++ server.js
@@ -2,18 +2,20 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-const MOVIESTORE = require('./movies-data.json')
-console.log(process.env.API_TOKEN)
+const helmet = require('helmet')
+const MOVIESTORE = require('./movies-data-small.json')
+
 const app = express()
 
 app.use(morgan('dev'))
-
+app.use(cors())
+app.use(helmet())
 
 app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN
     const authToken = req.get('Authorization')
     if (!authToken || authToken.split(' ')[1] !== apiToken) {
-      return res.status(401).json({ error: 'Unauthorized request' })
+      return res.status(401).json({ error: 'Try again' })
     }
     next()
   })
